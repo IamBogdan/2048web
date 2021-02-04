1995888449eda98560059f692cae619240bffcc8
unsky559 <6kostya@gmail.com>
2021-02-05 01:24:17 +0200

    hotfix

--- js/controller.js
+++ js/controller.js
@@ -28,24 +28,28 @@ function main() {
 
   document.addEventListener('keydown', function(event) {
     if(event.keyCode == 87 || event.keyCode == 38){
-      game.doGameMove("UP")
-      // new Block(game.generatePosition(), game.generateValue());
+      game.doGameMove("UP");
+      if(game.is_move){
+        new Block(game.generatePosition(), game.generateValue());
+      }
     }
     else if(event.keyCode == 83 || event.keyCode == 40){
       game.doGameMove("DOWN");
-      // new Block(game.generatePosition(), game.generateValue());
+      if(game.is_move){
+        new Block(game.generatePosition(), game.generateValue());
+      }
     }
     else if(event.keyCode == 65 || event.keyCode == 37){
       game.doGameMove("LEFT");
-      // new Block(game.generatePosition(), game.generateValue());
+      if(game.is_move){
+        new Block(game.generatePosition(), game.generateValue());
+      }
     }
     else if(event.keyCode == 68 || event.keyCode == 39){
       game.doGameMove("RIGHT");
-      //new Block(game.generatePosition(), game.generateValue());
-    }
-
-    if(game.is_move){
-      new Block(game.generatePosition(), game.generateValue());
+      if(game.is_move){
+        new Block(game.generatePosition(), game.generateValue());
+      }
     }
 
     console.log(board);
